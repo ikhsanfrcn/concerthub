2c6dabfc8bc8fce6a67cdd32483fe5cbf5ea0a13
ikhsanfrcn <ikhsanfrcn@gmail.com>
2025-04-10 15:59:04 +0700

    backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors"
+
+const PORT: number = 8000;
+const app: Application = express();
+app.use(express.json())
+app.use(cors())
+
+app.get('/api', (req: Request, res: Response) => {
+    res.status(200).send({ message: 'Welcome!'})
+})
+
+app.listen(PORT, () => {
+    console.log((`Server running http://localhost:${PORT}/api`));
+})
