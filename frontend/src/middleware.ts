28ccc91143e2653b78dfa621454777ca4908e06e
ikhsanfrcn <ikhsanfrcn@gmail.com>
2025-05-01 21:40:42 +0700

    middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,12 +1,28 @@
-import { getToken } from "next-auth/jwt";
+// import { getToken } from "next-auth/jwt";
+// import { NextRequest, NextResponse } from "next/server";
+
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+
+// export async function middleware(req: NextRequest) {
+//   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+
+//   if (!token && req.nextUrl.pathname === "/dashboard") {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
+
+//   return NextResponse.next();
+// }
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const data = await auth();
 
-  if (!token && req.nextUrl.pathname === "/dashboard") {
-    return NextResponse.redirect(new URL("/login", req.url));
+  if (data && req.nextUrl.pathname.startsWith('/login')) {
+     return NextResponse.redirect(new URL("/", req.url));
+  }
+  if (data && req.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
