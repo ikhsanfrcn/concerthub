735363f42210d606dae21b68ce030a4807732e1b
ikhsanfrcn <ikhsanfrcn@gmail.com>
2025-05-01 18:34:44 +0700

    generate prisma

--- backend/prisma/generated/prisma/index.js
+++ backend/prisma/generated/prisma/index.js
@@ -313,7 +313,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
